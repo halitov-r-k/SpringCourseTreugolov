d04fd722c932c4b16d44bf668484bfa75485b85f
halitov-r-k <halitov.r.k@gmail.com>
2022-06-14 08:33:12 +0500

    2022-06-14T08:30 add @Configuration

---
+++ src/spring_introduction/Config1.java
@@ -0,0 +1,10 @@
+//конфигурация проекта Java-code
+package spring_introduction;
+
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+@ComponentScan("spring_introduction") //указание пакета в котором сканировать компоненты
+public class Config1 {
+}
